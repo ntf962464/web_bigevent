1d5e1e630de4ce2e23c35aa8c4f8a2e22e69d41a
ntf962464 <739487668@qq.com>
2020-10-14 10:25:39 +0800

    bigevent

---
+++ assets/js/login.js
@@ -0,0 +1,40 @@
+$(function () {
+    // 点击去注册跳往注册页面
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show()
+    });
+    // 点击去登录跳往登录页面
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide()
+    });
+    // 自定义密码验证
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            // 注意选择器中间加个空格
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    });
+    // 注册触发post事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        var data = { username: $('.reg-box [name=username]').val(), password: $('.reg-box [name=password]').val() }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg(res.message);
+            $('#link_login').click();
+        })
+    })
+})
